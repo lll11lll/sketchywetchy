825b55ffa0707153656f7b308ea314be36fc2fea
Nick <nicholasgreiner1@gmail.com>
2023-03-06 21:26:39 -0500

    when hovering over squares in grid, makes them black!

--- script.js
+++ script.js
@@ -7,16 +7,27 @@
 const mainCont = document.querySelector('.container');
 
 // const gridBox = document.createElement('div');
-// gridBox.className = '.gridbox'
+
 // gridBox.textContent = 'This is a box!'
 //gridBox.style.cssText = 'justify-content: center; align-items: center; width: 50px; height: 50px;'
 
 for(let i = 0; i < 256; i++) {
     let gridBox = document.createElement('div');
-
+    gridBox.classList.add('gridbox')
     gridBox.style.cssText = 'border: solid 2px black;width: 50px; height: 50px; box-sizing: border-box;';
 
 
     mainCont.appendChild(gridBox);
-    console.log('Added gridbox to main container')
+    //console.log('Added gridbox to main container')
 };
+
+const gridBoxes = document.querySelectorAll('.gridbox');
+
+gridBoxes.forEach((gridBox) => {
+    gridBox.addEventListener('mouseover', () => {
+        gridBox.classList.add('blackout');
+    });
+    // gridBox.addEventListener('mouseout', () => {
+    //     gridBox.classList.remove('blackout');
+    // });
+});
