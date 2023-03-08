04c17cf8072fc0e70f1ca62eac7c6a4faf029b9b
Nick <nicholasgreiner1@gmail.com>
2023-03-07 19:24:42 -0500

    Finished size, reset, and cleaned JS code up

--- script.js
+++ script.js
@@ -11,41 +11,81 @@
 // Creating the grid for etch-a-sketch
 
 const mainCont = document.querySelector('.container');
-const colorPick = document.querySelector('.colorPicker')
+const colorPick = document.querySelector('.colorPicker');
+const sizePick = document.querySelector('.sizeSelector');
+const clearButton = document.querySelector('.cb');
 let myColor = '#000000'
-
-// const gridBox = document.createElement('div');
-
-// gridBox.textContent = 'This is a box!'
-//gridBox.style.cssText = 'justify-content: center; align-items: center; width: 50px; height: 50px;'
-
+let myValue = 'Small'
+let gridBoxes 
 
 
 colorPick.addEventListener('input', () => {
     myColor = colorPick.value;
 });
 
+sizePick.addEventListener('change', () => {
+    myValue = sizePick.value;
+    console.log(myValue);
+    mainCont.innerHTML = '';
+    generateGrid(myValue);
+});
+
+clearButton.addEventListener('click', () => {
+    gridBoxes.forEach((gridBox) => {
+        gridBox.classList.add('whiteBG');
+        gridBox.style.backgroundColor = ''
+    });
+});
+
+
+
+function updateGrid(gridBoxes) {
+    gridBoxes.forEach((gridBox) => {
+        gridBox.addEventListener('mouseover', () => {
+            gridBox.classList.remove('whiteBG');
+            gridBox.style.backgroundColor = `${myColor}`
+        });
+    });
+
+}
 
 // Small medium or large, small - 16x16, medium - 32x32, large - 64x64
-for(let i = 0; i < 256; i++) {
-    let gridBox = document.createElement('div');
-    gridBox.classList.add('gridbox')
-    gridBox.classList.add('whiteBG')
-    gridBox.style.cssText = ' border: solid 2px black; width: 50px; height: 50px; box-sizing: border-box;';
+function generateGrid(size) {
+    if (size === 'Small (16x16)') {
+        for(let i = 0; i < 256; i++) {
+            let gridBox = document.createElement('div');
+            gridBox.classList.add('gridbox')
+            gridBox.classList.add('whiteBG')
+            gridBox.style.cssText = ' border: solid 2px black; width: 50px; height: 50px; box-sizing: border-box;';
+            mainCont.appendChild(gridBox);
 
+            //console.log('Added gridbox to main container')
+        };
+    } else if (size === 'Medium (32x32)') {
 
-    mainCont.appendChild(gridBox);
-    //console.log('Added gridbox to main container')
-};
+        for(let i = 0; i < 1024; i++) {
+            let gridBox = document.createElement('div');
+            gridBox.classList.add('gridbox')
+            gridBox.classList.add('whiteBG')
+            gridBox.style.cssText = ' border: solid 2px black; width: 25px; height: 25px; box-sizing: border-box;';
+            mainCont.appendChild(gridBox);
+            
+            //console.log('Added gridbox to main container')
+        };
+    } else if (size === 'Large (64x64)') {
+        for(let i = 0; i < 4096; i++) {
+            let gridBox = document.createElement('div');
+            gridBox.classList.add('gridbox')
+            gridBox.classList.add('whiteBG')
+            gridBox.style.cssText = ' border: solid 2px black; width: 12.5px; height: 12.5px; box-sizing: border-box;';
+            mainCont.appendChild(gridBox);
+            
+            //console.log('Added gridbox to main container')
+        };
+    };
+    gridBoxes = document.querySelectorAll('.gridbox');
+    updateGrid(gridBoxes)
+}
 
-const gridBoxes = document.querySelectorAll('.gridbox');
+generateGrid('Small (16x16)')
 
-gridBoxes.forEach((gridBox) => {
-    gridBox.addEventListener('mouseover', () => {
-        gridBox.classList.remove('whiteBG');
-        gridBox.style.backgroundColor = `${myColor}`
-    });
-    // gridBox.addEventListener('mouseout', () => {
-    //     gridBox.classList.remove('blackout');
-    // });
-});
